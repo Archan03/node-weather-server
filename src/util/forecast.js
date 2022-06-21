8ee9f819487f8d5b857e6a0fcaeba7e42ad1f22e
Archan03 <67941059+Archan03@users.noreply.github.com>
2022-06-21 14:27:10 +0530

    error

--- src/util/forecast.js
+++ src/util/forecast.js
@@ -6,11 +6,11 @@ const forecast = (address, callback) => {
     request({url, json: true},(error,  {body }={}) =>{
         // console.log(error)
         if(error){
-            callback(undefined,'Unable to connect!!')
+            callback('Unable to connect!!',undefined)
 
         }
         else if(body.cod == 404){
-            callback(undefined,'Unable to find location!!')
+            callback('Unable to find location!!',undefined)
         }
         else{
             callbackString.value1= ('It is currently '+ (body.list[0].main.temp-273.15).toFixed(2) +' in '+body.city.name+' . And weather is '  +body.list[0].weather[0].description+'. Today high is '+(body.list[0].main.temp_max-273.15 ).toFixed(2)+' and low is '+(body.list[0].main.temp_min-273.15 ).toFixed(2))
